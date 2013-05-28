ce565638884c9fc8eb3a3abc9eca188964c19d49
Krzysztof A. Sobiecki <sobkas@gmail.com>
2013-05-28 02:53:08 +0200

    Next iteration of encode script

--- scripts/glc-encode.sh
+++ scripts/glc-encode.sh
@@ -19,16 +19,22 @@ audios=""
 au=""
 for i in `seq 1 $1`;
 do
-	i386-linux-gnu-glc-play $2 -o - -a $i|avconv -i - -y $tmp/audio$i.wav
+	i386-linux-gnu-glc-play $2 -o - -a $i|avconv -i - -y $tmp/audio_$i.wav
 	#audios='-i '$tmp'/audio'$i'.mkv '$audios
-	au=$tmp'/audio'$i'.wav '$au
+	au=$tmp'/audio_'$i'.wav '$au
 done
 #echo $au
 #echo $audios
+ls -lh $tmp
 i386-linux-gnu-glc-play $2 -o - -y 1|avconv -i - -y $tmp/video.mkv
 
 if [ x$3 != 'xkeep' ];then
-sox -m $au $tmp/audio-out.wav
+	if [ $1 == '1' ];then
+		mv $tmp/audio_1.wav $tmp/audio-out.wav
+	else
+		sox -m $au $tmp/audio-out.wav
+		rm $tmp/audio_*.wav
+	fi
 avconv -y -i $tmp/video.mkv -i $tmp/audio-out.wav -vcodec copy $2.mkv
 rm -r $tmp
 rm $2
